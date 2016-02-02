69d7e78a85b5c5b0cd4f10c4d9213bfafc7672d8
James Fenwick <j.fenwick@me.com>
2016-02-02 12:20:53 +0000

    Add SMS

--- textlocal/messages.py
+++ textlocal/messages.py
@@ -1,27 +1,95 @@
+"""
+Textlocal message classes.
+"""
 import base64
+from datetime import datetime
 
 
 class Message(object):
+    """
+    Base message class
+
+    Attributes:
+        message: The message content.
+        numbers: A List of numbers to send the message to.
+        group_id: The group_id to send the message to. Overrides numbers.
+        schedule_time: A datetime object specifying when to send the message.
+        optouts: A Boolean setting whether to check the recipients against an optout list.
+    """
 
     def __init__(self, message, numbers=None, group_id=None,
-                 schedule_time=None, optouts=None):
+                 schedule_time=None, optouts=False):
+        """
+        Returns a new Message object
+        """
+        # pylint: disable-msg=R0913
         self.message = message
+        if numbers is None and group_id is None:
+            raise Exception('numbers or group_id must be set.')
+        else:
+            self.numbers = numbers
+            self.group_id = group_id
         self.numbers = numbers
-        self.group_id = group_id
-        self.schedule_time = schedule_time
-        self.optouts = optouts
+        if schedule_time is not None and not isinstance(schedule_time, datetime):
+            raise TypeError("If set 'schedule_time' should be a 'datetime'.")
+        else:
+            self.schedule_time = schedule_time
+        if not isinstance(optouts, bool):
+            raise TypeError("If set 'optouts' must be a boolean.")
+        else:
+            self.optouts = optouts
+
+    def message_size(self):
+        """
+        Calculates the size of the message in bytes.
+        """
+        string = self.message.encode('utf8')
+        return len(string)
+
+    def __len__(self):
+        return self.message_size()
+
+
+class SMS(Message):
+    """
+    SMS message.
+
+    Attributes:
+        simple_reply: A Boolean setting whether or not to use the simple reply service.
+    """
+
+    def __init__(self, message, numbers=None, group_id=None,
+                 schedule_times=None, simple_reply=False):
+        """Returns a new SMS object."""
+        # pylint: disable-msg=R0913
+        super().__init__(message, numbers, group_id, schedule_times)
+        self.simple_reply = simple_reply
 
 
 class MMS(Message):
+    """
+    MMS message
+
+    Attributes:
+        url: A url giving the location of a media file.
+        image: A file-like object to base64 encode.
+    """
 
     def __init__(self, message, numbers=None, group_id=None,
-                 schedule_time=None, optouts=None, url=None, image=None):
-        super(__class__, self).__init__(message, numbers, group_id,
-                                        schedule_time, optouts)
+                 schedule_time=None, optouts=False, url=None, image=None):
+        """
+        Returns new MMS object
+        """
+        # pylint: disable-msg=R0913
+        super().__init__(message, numbers, group_id, schedule_time, optouts)
         self.url = url
         if image is not None:
-            self.encoded_image = base64.b64encode(image.read())
+            try:
+                self.encoded_image = base64.b64encode(image.read())
+            except AttributeError as e:
+                raise TypeError("If 'image' is used it must"
+                                " be a file-like object.") from e
 
     def message_size(self):
-        s = self.message.encode('utf8')
-        return len(s)
+        size = super().message_size()
+        return size + len(self.encoded_image)
--- textlocal/test/test_messages.py
+++ textlocal/test/test_messages.py
@@ -1,7 +1,81 @@
 import base64
+from datetime import datetime
 import unittest
 
-from textlocal.messages import MMS
+from textlocal.messages import Message, SMS, MMS
+
+
+class MessageTest(unittest.TestCase):
+
+    def test_init_numbers_or_group_id(self):
+        with self.assertRaises(Exception):
+            message = Message('message', numbers=None, group_id=None)
+
+    def test_init_numbers(self):
+        message_text = 'message'
+        number = '4470123456789'
+        message = Message(message_text, numbers=[number, ])
+        self.assertEqual(message.message, message_text)
+        self.assertTrue(type(message.numbers), list)
+        self.assertIn(number, message.numbers)
+
+    def test_init_group_id(self):
+        message_text = 'message'
+        group_id = '4'
+        message = Message(message_text, group_id=group_id)
+        self.assertEqual(message.message, message_text)
+        self.assertEqual(message.group_id, group_id)
+
+    def test_init_schedule_time(self):
+        message_text = 'message'
+        number = '4470123456789'
+        with self.assertRaises(TypeError):
+            message = Message(message_text, [number, ], schedule_time="time")
+        schedule_time = datetime.today()
+        message = Message(message_text, numbers=[number, ],
+                          schedule_time=schedule_time)
+        self.assertEqual(message.schedule_time, schedule_time)
+
+    def test_init_optouts(self):
+        message_text = 'message'
+        number = '4470123456789'
+        with self.assertRaises(TypeError):
+            message = Message(message_text, numbers=[number, ], optouts='true')
+        optouts = True
+        message = Message(message_text, numbers=[number, ], optouts=True)
+        self.assertTrue(message.optouts)
+
+    def test_message_size(self):
+        message_text = 'message'
+        number = '4470123456789'
+        message = Message(message_text, [number, ])
+        self.assertEqual(message.message_size(), 7)
+        self.assertEqual(len(message), 7)
+        message.message = '𠜎'
+        self.assertEqual(message.message_size(), 4)
+        self.assertEqual(len(message), 4)
+
+
+class SMSTest(unittest.TestCase):
+
+    def test_init_simple_reply(self):
+        message_text = 'message'
+        number = '4470123456789'
+        simple_reply = True
+        message = SMS(message_text, numbers=[number, ],
+                      simple_reply=simple_reply)
+        self.assertTrue(message.simple_reply)
+
+    def test_message_size(self):
+        message_text = 'message'
+        number = '4470123456789'
+        message = SMS(message_text, [number, ])
+        self.assertEqual(message.message_size(), 7)
+        self.assertEqual(len(message), 7)
+        message.message = '𠜎'
+        self.assertEqual(message.message_size(), 4)
+        self.assertEqual(len(message), 4)
+
 
 class MMSTest(unittest.TestCase):
 
@@ -9,15 +83,16 @@ class MMSTest(unittest.TestCase):
         message_text = 'message'
         number = '4470123456789'
         url = 'http://example.com'
-        message = MMS(message_text, [number,], url=url)
+        message = MMS(message_text, [number, ], url=url)
         self.assertEqual(message.url, url)
 
     def test_init_encoded_image(self):
         message_text = 'message'
         number = '4470123456789'
-        file_name = __file__
-        with open(file_name, 'rb') as test_file:
-            message = MMS(message_text, [number,], image=test_file)
+        with self.assertRaises(TypeError):
+            message = MMS(message_text, [number, ], image='test_file')
+        with open(__file__, 'rb') as test_file:
+            message = MMS(message_text, [number, ], image=test_file)
             test_file.seek(0)
             encoded_file = base64.b64encode(test_file.read())
         self.assertEqual(message.encoded_image, encoded_file)
@@ -25,10 +100,12 @@ class MMSTest(unittest.TestCase):
     def test_message_size(self):
         message_text = 'message'
         number = '4470123456789'
-        message = MMS(message_text, [number,])
-        self.assertEqual(message.message_size(), 7)
-        message.message = '𠜎'
-        self.assertEqual(message.message_size(), 4)
+        with open(__file__, 'rb') as test_file:
+            message = MMS(message_text, [number, ], image=test_file)
+            test_file.seek(0)
+            encoded_file = base64.b64encode(test_file.read())
+        self.assertEqual(message.message_size(), 7 + len(encoded_file))
+        self.assertEqual(len(message), 7 + len(encoded_file))
 
 if __name__ == '__main__':
     unittest.main()
