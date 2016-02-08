b223ef17bc0073b7c65c44f8e0eb54a8be9d4961
James Fenwick <j.fenwick@me.com>
2016-02-08 17:15:52 +0000

    Rename textlocal/account.py -> textlocal/contacts.py

--- textlocal/test/test_contacts.py
+++ textlocal/test/test_contacts.py
@@ -1,6 +1,6 @@
 import unittest
 
-import textlocal.account
+import textlocal.contacts
 
 
 class TemplateTest(unittest.TestCase):
@@ -8,7 +8,7 @@ class TemplateTest(unittest.TestCase):
     def test_template(self):
         title = 'title'
         body = 'body'
-        template = textlocal.account.Template(title, body)
+        template = textlocal.contacts.Template(title, body)
         self.assertEqual(template.title, title)
         self.assertEqual(template.body, body)
 
@@ -17,7 +17,7 @@ class GroupTest(unittest.TestCase):
 
     def test_group_with_only_name(self):
         name = 'name'
-        group = textlocal.account.Group(name)
+        group = textlocal.contacts.Group(name)
         self.assertEqual(group.name, name)
         self.assertEqual(group.id, None)
         self.assertEqual(group.size, None)
@@ -25,13 +25,13 @@ class GroupTest(unittest.TestCase):
     def test_group_with_group_id(self):
         name = 'name'
         group_id = 1
-        group = textlocal.account.Group(name, group_id)
+        group = textlocal.contacts.Group(name, group_id)
         self.assertEqual(group.id, group_id)
 
     def test_group_with_size(self):
         name = 'name'
         size = 1
-        group = textlocal.account.Group(name, size=size)
+        group = textlocal.contacts.Group(name, size=size)
         self.assertEqual(group.size, size)
 
 
